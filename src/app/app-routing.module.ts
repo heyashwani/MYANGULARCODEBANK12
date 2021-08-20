6d71b59960ac99822438eac844d9a1e7afd87a8b
bani006 <ashwani.kumar@techugo.com>
2021-08-20 13:00:35 +0530

    sda

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CountryDropdownComponent } from './country-dropdown/country-dropdown.component';
+import { SpeechRecognitionComponent } from './speech-recognition/speech-recognition.component';
 
 const routes: Routes = [
   {
@@ -11,7 +12,11 @@ const routes: Routes = [
   {
     path:"country-dropdown",
     component:CountryDropdownComponent
-  }
+  },
+  {
+    path:"speech-recognition",
+    component:SpeechRecognitionComponent
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { AppComponent } from './app.component';
 import { CountryDropdownComponent } from './country-dropdown/country-dropdown.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SpeechRecognitionComponent } from './speech-recognition/speech-recognition.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountryDropdownComponent
+    CountryDropdownComponent,
+    SpeechRecognitionComponent,
+    
   ],
   imports: [
     BrowserModule,
